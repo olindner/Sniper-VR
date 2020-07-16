c58a51d51dcf45182ff040bdb80b1e184f53fc64
Ori Lindner <olindner@umich.edu>
2020-07-16 17:45:40 -0400

    Started investigating disabling left eye when scoped in. WIP.

--- Assets/Scripts/BulletExplode.cs
+++ Assets/Scripts/BulletExplode.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEngine;
-using UnityEngine.SearchService;
+﻿using UnityEngine;
 
 public class BulletExplode : MonoBehaviour
 {
